2db99c039b0a92bfcaf3b646e0e0557fff7bb2c6
danielalfb <danielalfbarbosa@gmail.com>
2021-06-02 19:58:55 -0300

    método post de produto implementado, teste do get /produto/id e get /departamento

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = new Router();
+
 const produtos = [
   {
     id: 1,
@@ -50,5 +51,53 @@ router.get('/produtos', (req, res) => {
     return res.status(404).json({ message: 'Nenhum produto cadastrado.' });
   }
 });
+
+// router.get('/produtos/:id', (req, res) => {
+//   const produtoId = req.params.id;
+//   const produto = produtos.find((c) => c.id === parseInt(produtoId));
+//   if (!produto) res.status(404).json({ err: 'Produto não existe.' });
+//   return res.status(200).json(produto);
+// });
+
+router.post('/produtos', (req, res) => {
+  const produto = {
+    id: produtos.length + 1,
+    nome: req.body.nome,
+    preco: req.body.preco,
+    qtd_estoque: req.body.qtd_estoque,
+    disponivel: req.body.disponivel,
+    em_destaque: req.body.em_destaque,
+    id_dept: req.body.id_dept,
+    nome_dept: req.body.nome_dept,
+  };
+
+  if (
+    !produto.nome ||
+    produto.preco === 0 ||
+    !produto.qtd_estoque ||
+    !produto.disponivel ||
+    !produto.em_destaque ||
+    !produto.id_dept ||
+    !produto.nome_dept
+  ) {
+    return res.status(400).json({ err: 'Preenchimento incorreto.' });
+  } else {
+    produtos.push(produto);
+    return res.status(200).json(produto);
+  }
+});
+
+router.put('/produtos/:id', (req, res) => {});
+
+// router.get('/departamentos', (req, res) => {
+//   const departamentos = [];
+//   for (let produto of produtos) {
+//     let departamento = { id: produto.id_dept, nome: produto.nome_dept };
+//     departamentos.push(departamento);
+//   }
+//   return res.status(200).json(departamentos);
+// });
+
+router.get('/departamentos/:id', (req, res) => {});
 
 module.exports = router;
